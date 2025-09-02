bf2c5036c217a67b715323a689f269f31a948a26
eclipsevoidd <gustavs.lacis@sk.lvt.lv>
2025-09-02 11:42:04 +0300

    Skaitļa uzmešana realizēta

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -1,9 +1,15 @@
 package lvt;
+import java.util.Random;
 
 public class Programma {
 
 	public static void main(String[] args) {
-
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6) + 1;
+		System.out.println("Uzkrita skaitlis " + skaitlis);
+		
+		
 	}
 
 }
