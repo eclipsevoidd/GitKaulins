46124ac07325cc1f71334b5b358b1a72889d6b73
eclipsevoidd <gustavs.lacis@sk.lvt.lv>
2025-09-02 12:15:23 +0300

    Ieviesta iespēja ievadīt metienu skaitu

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -1,14 +1,20 @@
 package lvt;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Programma {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6) + 1;
-		System.out.println("Uzkrita skaitlis " + skaitlis);
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes = scan.nextInt();
+		for (int i = 0; i <= reizes; i++) {
+			skaitlis = rand.nextInt(6) + 1;
+			System.out.println("Uzkrita skaitlis " + skaitlis);
+		}
+		scan.close();
 		
 	}
 
